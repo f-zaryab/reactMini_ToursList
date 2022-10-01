513d8ff9be14a1c8f0358c5a165bc69c36ba6ba6
f-zaryab <zaryabfarooqui@gmail.com>
2022-10-02 02:37:57 +0500

    Updates

---
+++ precache-manifest.465b58e0d0905058536590b968f9626f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2727052e3700cedccd48f120de6e34",
+    "url": "/reactMini_ToursList/index.html"
+  },
+  {
+    "revision": "4257f128107a4686af8d",
+    "url": "/reactMini_ToursList/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/reactMini_ToursList/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactMini_ToursList/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4257f128107a4686af8d",
+    "url": "/reactMini_ToursList/static/js/main.a9b75730.chunk.js"
+  },
+  {
+    "revision": "ec3d3b0c7fa2c82c9b4b",
+    "url": "/reactMini_ToursList/static/js/runtime-main.d07a489c.js"
+  }
+]);
